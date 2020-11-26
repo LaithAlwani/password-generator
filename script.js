1712027a4cb97a70c9803ff33b71c8098b7cd87a
Laith Alwani <laithalwani@gmail.com>
2020-11-26 00:41:37 -0500

    pass can now be generated with all criteria's checked includes at least 1 of each character asked for

--- script.js
+++ script.js
@@ -1,44 +1,81 @@
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+var specialChars = ["!","@","#","$","%","^","&","*","(",")"];
 
-var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':',
-    ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-
-var password = "";
 
 function GeneratePassword() {
-    password = "";
+    //variable declerations
+    var includedChars = [];
+    var forcedChars = [];
+    var password = [];
     var passwordLength = prompt("how long would you like the length of the password to be");
-    //check for passwordLength to make sure its a number between 8 and 128
-    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-        //check if passwordLength is a string
-        if (isNaN(passwordLength)) {
-            passwordLength = prompt("please enter a number between 8 and 128 not a String");
-        } else {
-            passwordLength = prompt("please enter a number between 8 and 128");
-        }
+
+    //check for passwordLength to make sure its a number between 8 and 128 and it's not a string
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) { 
+        passwordLength = prompt("please enter a number between 8 and 128");   
     }
 
-    // var includeLowerCase = confirm("does the password have Lowercase letters?")
-    // var includeUpperCase = confirm("does the password have Uppercase letters?")
-    // var includeNmbers = confirm("does the password have Numbers letters?")
-    // var includeSpecialCharacters = confirm("does the password have Special Characters letters?")
-
-    for(var i=0; i<passwordLength; i++){
-        var arrIndex = Math.floor(Math.random()*26);
-        password += lowercaseLetters[arrIndex];
+    //do while loop to make sure at least one cirteria is choosen
+    do{
+        var includeLC = confirm("include Lowercase letters?");
+        var includeUC = confirm("include Uppercase letters?");
+        var includeNum = confirm("include Numbers?");
+        var includeSC = confirm("include Special Chars?");
     }
-    document.getElementById("passwordText").innerHTML = password;
+        
+    while(!includeLC & !includeUC & !includeNum & !includeSC);
+
+    //checking password criteria
+    if(includeLC){
+        includedChars = includedChars.concat(lowerCase);
+        forcedChars.push(lowerCase[Math.floor(Math.random()*lowerCase.length)])
+        console.log(forcedChars);
+    }
+
+    
+    if(includeUC){
+        includedChars = includedChars.concat(upperCase);
+        forcedChars.push(upperCase[Math.floor(Math.random()*upperCase.length)])
+        console.log(forcedChars);
+    }
+    if(includeNum){
+        includedChars = includedChars.concat(numbers);
+        forcedChars.push(numbers[Math.floor(Math.random()*numbers.length)])
+        console.log(forcedChars);
+    }
+
+    if(includeSC){
+        includedChars = includedChars.concat(specialChars);
+        forcedChars.push(specialChars[Math.floor(Math.random()*specialChars.length)])
+        console.log(forcedChars);
+    }
+
+    for(var i= 0; i<passwordLength;i++){
+        password.push(includedChars[Math.floor(Math.random()*includedChars.length)]) ;
+    }
+    
+    //spliced the last few elemets of the password and push the froced chars to make sure it includes the 
+    // all of the required characters
+    password.splice(password.length-forcedChars.length, forcedChars.length);
+    for(var i=0; i<forcedChars.length;i++){
+        password.push(forcedChars[i]);
+    }
+
+    //string variable to display the password
+    var thePassword = "";
+
+    
+    //converted the password array into a string
+    for(var i=0; i<passwordLength;i++){
+        akherKalam += password[i];
+    }
+    //display the password in the HTML file
+    document.getElementById("passwordText").innerHTML = thePassword;
     
 }
 
-
 // when the generate button is click the application starts.
-document.getElementById("generate").addEventListener("click", function () {
+document.getElementById("generate").addEventListener("click",  function(){
     GeneratePassword();
 });
