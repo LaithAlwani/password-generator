8819fa0a7363f7df52d6f7b4db3abe32fa021365
Laith Alwani <laithalwani@gmail.com>
2020-11-28 20:28:55 -0500

    renamed variables, classes and tags, added comments

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ var specialChars = ["!","@","#","$","%","^","&","*","(",")"];
 function GeneratePassword() {
     //variable declerations
     var includedChars = [];
-    var forcedChars = [];
+    var forcedChars = []; //holds the characters to make sure at least 1 of each choosed criteria are present in the password
     var passwordArray = [];
-    var passwordLength = prompt("how long would you like the length of the password to be");
+    var passwordLength = prompt("please enter the desired password length (between 8 and 128 characters)");
     var password = ""; //string variable to display the password
 
     //check for passwordLength to make sure its a number between 8 and 128 and it's not a string
     while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) { 
-        passwordLength = prompt("please enter a number between 8 and 128");   
+        passwordLength = prompt("Make sure the password length is between 8 and 128 characters");   
     }
 
     //do while loop to make sure at least one cirteria is choosen
     do{
-        var includeLC = confirm("would you like the password to include Lowercase letters?");
-        var includeUC = confirm("would you like the password to include Uppercase letters?");
-        var includeNum = confirm("would you like the password to include numbers?");
-        var includeSC = confirm("would you like the password to include special charaters?");
+        var includeLC = confirm("click Ok to include Lowercase letters in your password");
+        var includeUC = confirm("click Ok to include Uppercase letters in your password");
+        var includeNum = confirm("click Ok to include numbers in your password");
+        var includeSC = confirm("click Ok to include special characters in your password");
         if(!includeLC & !includeUC & !includeNum & !includeSC){
             alert("Please choose at least one criteria")
         }
@@ -32,7 +32,7 @@ function GeneratePassword() {
     //add included character to the includeChars array
     function addCharacters (characters){
         includedChars = includedChars.concat(characters);
-        forcedChars.push(characters[Math.floor(Math.random()*characters.length)])
+        forcedChars.push(characters[Math.floor(Math.random()*characters.length)]) //
     }
 
     //adding characters to includedChars, the password will be choosen from the new array.
@@ -49,27 +49,24 @@ function GeneratePassword() {
     if(includeSC){
         addCharacters(specialChars);
     }
-
+    
+    //characters are pushed into the password array
     for(var i= 0; i<passwordLength;i++){
         passwordArray.push(includedChars[Math.floor(Math.random()*includedChars.length)]) ;
     }
     
-    //splice the last few elemets of the password and push the froced chars to make sure it includes the 
-    // all of the required characters
+    //splice the last few elemets of the password and push the froced chars to make sure it includes at least 1 of each desired criteria 
     passwordArray.splice((passwordArray.length-forcedChars.length), forcedChars.length);
     for(var i=0; i<forcedChars.length;i++){
         passwordArray.push(forcedChars[i]);
     }
 
-    
-
-    
-    //converted the password array into a string
+    //convert the password array into a string
     for(var i=0; i<passwordLength;i++){
         password += passwordArray[i];
     }
     //display the password in the HTML file
-    document.getElementById("passwordText").innerHTML = password;
+    document.getElementById("passwordText").textContent = password;
     
 }
 
